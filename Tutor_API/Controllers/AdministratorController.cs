5e309f1b46d4a79b35671a26f52a016b5b1da265
Emir Čajić <emir.cajic@edu.fit.ba>
2019-03-12 14:32:52 +0100

    Prvi Commite(PostAdministrator)

---
+++ Tutor_API/Controllers/AdministratorController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Tutor_API.Models;
+
+namespace Tutor_API.Controllers
+{
+    public class AdministratorController : ApiController
+    {
+        private TutorEntities db = new TutorEntities();
+
+        [HttpGet]
+        [ResponseType(typeof(List<Administrator_SelectAll>))]
+        public List<Administrator_SelectAll> GetAdministrator() {
+
+            return db.tsp_Administrator_SelectAll().ToList();
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAdministrator(int id)
+        {
+            Administrator_SelectOne admin = db.tsp_Administrator_SelectOne(id).FirstOrDefault();
+            if (admin==null)
+            {
+                return NotFound();
+            }
+
+            return Ok(admin);
+        }
+
+        [HttpGet]
+        [Route("api/Administrator/SearchByName/{name?}")]
+        public List<Administrator_NameSelect> SearchByName(string name = "")
+        {
+            return db.tsp_Administrator_SelectByImePrezime(name).ToList();
+        }
+
+        [HttpPost]
+        public IHttpActionResult PostAdministrator(Administrator a) {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
+           a.AdministratorId =db.tsp_Administrator_Insert(a.Ime, a.Prezime, a.DatumDodavanja, a.Email, a.Telefon, a.KoriniskoIme, a.LozinkaHash, a.LozinkaSalt);
+
+            return CreatedAtRoute("DefaultApi", new { id = a.AdministratorId }, a);
+        }
+    }
+}
---
+++ Tutor_API/Models/AdministratorExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tutor_API.Models
+{
+    public partial class Administrator
+    {
+        public string Email { get; set; }
+
+        public string Telefon { get; set; }
+
+        public string KoriniskoIme { get; set; }
+
+        public string LozinkaHash { get; set; }
+
+        public string  LozinkaSalt { get; set; }
+    }
+}
